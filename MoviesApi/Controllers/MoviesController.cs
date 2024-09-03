6f51fa44527b8ce78a21a73844a73b3505483f3b
Hamed Ahmed <ha9597ma@gmail.com>
2024-09-03 12:41:13 +0300

    Initiate project

---
+++ MoviesApi/Controllers/MoviesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MoviesApi.Dtos;
+using MoviesApi.Models;
+
+namespace MoviesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromForm]MovieDto dto)
+        {
+            using var dataStream = new MemoryStream();
+
+            await dto.Poster.CopyToAsync(dataStream);
+
+            var movie = new Movie
+            {
+                GenreId = dto.GenreId,
+                Title = dto.Title,
+                Poster = dataStream.ToArray(),
+                Rate = dto.Rate,
+                Storeline = dto.Storeline,
+                Year = dto.Year
+            };
+
+            await _context.AddAsync(movie);
+            _context.SaveChanges();
+
+            return Ok(movie);
+        }
+    }
+}
---
+++ MoviesApi/Dtos/GenreDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesApi.Dtos
+{
+    public class GenreDto
+    {
+        [MaxLength(length:100)]
+        public string Name { get; set; }
+    }
+}
